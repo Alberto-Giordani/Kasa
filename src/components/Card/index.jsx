6288c97869007b43e62c0e6d09aed138b4ad17b9
Alberto Giordani <albertogiordani@ymail.com>
2025-04-04 11:19:57 +0200

    Mise à jour des commentaires

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -6,7 +6,8 @@
 // Il utilise le composant Link de react-router pour naviguer vers la page de détail de l'annonce.
 // Les informations affichées incluent une image, le titre et la localisation de l'annonce.
 //
-// Le composant reçoit en prop un objet "annonce" qui contient les informations nécessaires.
+// Le composant reçoit en prop un objet "annonce" qui contient les informations nécessaires -
+// que nous irons ensuite récupérer depuis 'data/annonces.json'.
 //
 import { Link } from 'react-router';
 import './Card.scss';
