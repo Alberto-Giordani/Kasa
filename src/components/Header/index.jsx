29e13f70d4772561a64347cc50fd7817cb51d901
Alberto Giordani <albertogiordani@ymail.com>
2025-04-05 10:42:09 +0200

    Ajout du favicon avec useEffect dans le Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
+import favicon from '../../assets/images/kasa.svg';
 import logo from '../../assets/images/logo.svg';
 import logo_petit from '../../assets/images/logo_petit.svg';
 import Nav from '../Nav';
 import './Header.scss';
 
 function Header() {
+
+    useEffect(() => {
+        const link = document.createElement('link');
+        link.rel = 'icon';
+        link.href = favicon;
+        document.head.appendChild(link);
+    }, []);
+
     return (
         <header className="header">
             <picture>
